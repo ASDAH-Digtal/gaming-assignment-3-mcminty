d16bc5d9547a9757d4d3a8104dc50cac2a9b7aa1
mcminty <sione_hav@student.asdah.school.nz>
2023-08-11 15:00:13 +1200

    Added collision shape to Tilemaps
    Signed-off-by: mcminty <sione_hav@student.asdah.school.nz>

--- character/player.gd
+++ character/player.gd
@@ -3,23 +3,32 @@ extends CharacterBody2D
 
 @export var speed : float = 200.0
 @export var jump_velocity : float = -200.0
+@export var double_jump_velocity : float = -100
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var has_double_jump : bool = false
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = jump_velocity
+		# Handle Jump.
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			#Normal jump from floor
+			velocity.y = jump_velocity
+		elif not has_double_jumped:
+			# Double jump in air
+			velocity.y = double_jump_velocity
+			has_double_jump = true
+			
+
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * speed
 	else:
